fe6f75f59be982145857902caf11c0fc0c8c3584
Wan Zulmuhammad Harith <31917565+wzharith@users.noreply.github.com>
2021-09-30 07:54:33 +0800

    init commit

---
+++ library-front/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookFormComponent } from './book-form/book-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BookListComponent,
+    BookFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
