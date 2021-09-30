5fd418d020e8e93290fb67826e3cce07a3943a75
Wan Zulmuhammad Harith <31917565+wzharith@users.noreply.github.com>
2021-09-30 08:34:33 +0800

    add routing

--- library-front/src/app/app-routing.module.ts
+++ library-front/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookFormComponent } from './book-form/book-form.component';
+import { BookListComponent } from './book-list/book-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'add', component: BookFormComponent },
+  { path: 'list', component: BookListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
